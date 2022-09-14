e314312b707f91c081e9314ce5a407611ecb207b
Areeb Arshad <59660097+AreebMughal@users.noreply.github.com>
2022-09-14 23:21:33 +0500

    Stemming step1a, step1b, step1b_b, step1c

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ from scipy.sparse import csr_matrix
 from nltk.stem import PorterStemmer
 
 word_stemmer = PorterStemmer()
+
+# from nltk.corpus import stopwords
+# import nltk
+# nltk.download('stopwords')
+# stopWords = set(stopwords.words('english'))
+# print(stopWords)
+
+tweets = np.array(["Doubt thou the, stars are fire",
+                   "Doubt Truth to be a liar",
+                   "But never doubt I love.",
+                   "I love watching this movie",
+                   "I know it's her car."])
 # twitter_df = pd.read_csv('./data.csv')
+# tweets = twitter_df.head(20)['clean_text'].to_numpy()
+# print(len(tweets))
 #
 # tweets = twitter_df['clean_text']
 # twitter_df['length'] = twitter_df['clean_text'].str.len()
@@ -17,16 +31,27 @@ word_stemmer = PorterStemmer()
 
 # tweets = twitter_df['clean_text'].tolist()
 
-tweets = np.array(["Doubt thou the, stars are fire",
-                   "Doubt Truth to be a liar",
-                   "But never doubt I love.",
-                   "I love watching this movie",
-                   "I know it's her car."])
 
-stop_words = {'i', 'me', 'to', 'my', 'myself', 'we', 'our', 'ours', 'your', 'you', 'yourself', 'ourselves',
-              'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'herself',
-              'it', 'its', 'hers', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what',
-              'which', 'who', 'whom', 'this', 'a', 'be', 'the', 'are'}
+# stop_words = {'i', 'me', 'to', 'my', 'myself', 'we', 'our', 'ours', 'your', 'you', 'yourself', 'ourselves',
+#               'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'herself',
+#               'it', 'its', 'hers', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what',
+#               'which', 'who', 'whom', 'this', 'a', 'be', 'the', 'are'}
+
+stop_words = {'under', 'very', 'shan', 'have', 'wouldn', 'because', 'as', "isn't", "didn't", 'over', 'me', 'into',
+              'after', "needn't", "shouldn't", 'she', 'him', 'about', 'these', 'when', 'so', 'hers', 'being', "doesn't",
+              'her', 'how', 'too', 'their', 'once', 'at', 'm', 'whom', 'through', 'it', 'does', 'was', 'this', 'than',
+              'were', 'off', 's', 'while', "you're", 'by', 'a', 'those', 'both', 'not', 'he', 'having', 'here', 've',
+              'y', 'i', 'ain', 'who', "couldn't", 'few', 'other', 'only', 'should', "she's", 'for', 'isn', "wasn't",
+              'o', 'such', 'yourself', 'himself', 'ma', 'to', "won't", 'same', 'will', 'do', 'needn', 'that', 'on',
+              'down', 're', "should've", 'hasn', 'doing', 'been', 'nor', 'didn', "haven't", 'weren', 'don', 'had',
+              'from', 'they', 'ours', 'which', 'doesn', 'theirs', 'is', 'his', "mustn't", 'or', 'again', 'yourselves',
+              'did', 'won', "it's", 'aren', 'd', 'with', 'myself', 'our', 'what', 'has', 'why', "aren't", 'of', 'all',
+              'any', 'can', 'out', 'haven', 'there', 'between', 'itself', 'yours', 'each', 'no', 'where', "mightn't",
+              "hasn't", 'above', 'shouldn', 'herself', 'are', 'am', "you'll", 'themselves', 'we', "weren't", 'you',
+              'my', 'the', "you'd", "you've", 'during', 'up', 'its', 'mustn', "that'll", 'in', 'more', 'll', "hadn't",
+              'wasn', 'hadn', 'mightn', 'now', 'an', 'couldn', 'most', 'before', 'further', 'them', 'until', 'and',
+              'ourselves', "wouldn't", 'just', 'if', 'then', 'below', 'against', 'own', "don't", 'be', 'some', "shan't",
+              'your', 't', 'but'}
 
 
 # special_characters = set('!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~')
@@ -42,7 +67,7 @@ def data_preprocessing(list_of_lines):
         line = " ".join(word for word in line.split() if word not in list(stop_words))
 
         # apply stemming
-        line = " ".join(word_stemmer.stem(word) for word in line.split())
+        # line = " ".join(word_stemmer.stem(word) for word in line.split())
 
         list_of_lines[index] = line
 
@@ -68,7 +93,7 @@ def remove_stop_words(data):
     return data
 
 
-def apply_stemming(data):
+def apply_builtin_stemming(data):
     for index, line in enumerate(data):
         line = " ".join(word_stemmer.stem(word) for word in line.split())
         data[index] = line
@@ -86,20 +111,109 @@ def find_unique_words(data):
     return sorted(list(unique_words))
 
 
-print('\n\n Before=>', tweets)
+vowels = np.array(['a', 'e', 'i', 'o', 'u'])
+
+
+# word = 'sing'
+
+
+def step1a(word):
+    new_word = ''
+    if 'sses' in word[-4:]:
+        new_word = word[0:-2]
+    elif 'ies' in word[-3:]:
+        new_word = word[0:-2]
+    elif 'ss' in word[-2:]:
+        new_word = word
+    elif 's' in word[-1:]:
+        new_word = word[0:-1]
+
+    return new_word
+
+
+def step1b(word):
+    new_word = word
+    if 'eed' in word[-3:]:
+        pass
+    elif 'ed' in word[-2:]:
+        res = set(True for v in vowels if v in word[0:-2])
+        if True in res:
+            new_word = word[0:-2]
+    elif 'ing' in word[-3:]:
+        res = set(True for v in vowels if v in word[0:-3])
+        if True in res:
+            new_word = word[0:-3]
+
+    return new_word
+
+
+def step1b_b(word):
+    new_word = word
+    if 'at' in word[-2:] or 'bl' in word[-2:] or 'iz' in word[-2:]:
+        new_word += 'e'
+    elif ((word[-2:-1] not in vowels and word[-1:] not in vowels) and
+          not (word[-1:] == 'l' or word[-1:] == 's') or word[-1:] == 'z'):
+        new_word = word[0:-1]
+
+    return new_word
+
+def step1c(word):
+    new_word = word
+    is_contain_vowel = set(True for v in vowels if v in word[0:-1])
+    if is_contain_vowel and word[-1:] == 'y':
+        new_word = word[0:-1] + 'i'
+
+    return new_word
+
+
+print(step1b_b('hopp'))
+print(step1b_b('tann'))
+print(step1b_b('fall'))
+print(step1b_b('hiss'))
+print(step1b_b('fizz'))
+
+print(step1c('happy'))
+print(step1c('sky'))
+
+# print(step1b('motoring'))
+
+
+
+
+def my_stemmer(data):
+    for index, line in enumerate(data):
+        new_line = ''
+        for word in line.split():
+            word = step1a(word)
+            word = step1b(word)
+            word = step1b_b(word)
+            word = step1c(word)
+
+
+#
+# word = 'caresses'
+# print(word[0:-2])
+# word = 'ponies'
+# print(word[-3:], word[0:-2])
+# word = 'caress'
+# print(word[-2:], word)
+# word = 'cats'
+# print(word[-1:], word[0:-1])
+#
+
+# print('\n\n Before=>', tweets)
 
 # tweets = remove_punctuation_specChar_digit(tweets)
 # tweets = remove_stop_words(tweets)
-# tweets = apply_stemming(tweets)
+# tweets = apply_builtin_stemming(tweets)
 # all_unique_words = find_unique_words(tweets)
 
-tweets, all_unique_words = data_preprocessing(tweets)
+# tweets, all_unique_words = data_preprocessing(tweets)
 
-print(all_unique_words)
-print('\n\n After=>', tweets)
+# print(all_unique_words)
+# print('\n\n After=>', tweets)
 
-print(word_stemmer.stem('movies'))
-
+# print(word_stemmer.stem('movies'))
 
 
 def UniqueWords(data):
